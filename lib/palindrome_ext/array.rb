981c66791f80d0e99ea17083765e3c3054a697ba
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-09 15:34:23 -0700

    run rubocop on array.rb && array_spec.rb

--- lib/palindrome_ext/array.rb
+++ lib/palindrome_ext/array.rb
@@ -1,7 +1,10 @@
-require 'palindrome_ext/string'
+# frozen_string_literal: true
 
+require "palindrome_ext/string"
+
+# Overrides Default Array Class by adding 'palindrome?' method
 class Array
   def palindrome?
-    self === self.reverse
+    self == reverse
   end
 end
