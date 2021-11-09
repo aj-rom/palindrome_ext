c2b93881b9aba68142826ff744ac59e074e9e03f
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-09 13:48:43 -0700

    add strict option, for palindromes with matching case, whitespace, and punctuation

--- lib/palindrome_ext/string.rb
+++ lib/palindrome_ext/string.rb
@@ -1,8 +1,8 @@
 class String
-  def palindrome?
+  def palindrome?(strict = false)
     return false if length < 2
-
-    str = self.downcase
+    str = self
+    str = str.downcase.tr('^a-z0-9', '') unless strict
     str == str.reverse
   end
 end
