6ed6ffb1c62fc555ccc9f63f66cf8543c741d9e2
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-09 16:33:02 -0700

    fix up style of relative require

--- lib/palindrome_ext.rb
+++ lib/palindrome_ext.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'palindrome_ext/version'
+require_relative "palindrome_ext/version"
 require_relative "palindrome_ext/string"
 require_relative "palindrome_ext/integer"
 require_relative "palindrome_ext/array"
