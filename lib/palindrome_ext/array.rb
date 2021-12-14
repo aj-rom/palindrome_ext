5f90421630fecafedc2d9c610d0b9bad554a06ef
A.J. Romaniello <ajiellodev@gmail.com>
2021-12-14 02:44:48 -0700

    feat(array): add strict option

--- lib/palindrome_ext/array.rb
+++ lib/palindrome_ext/array.rb
@@ -4,8 +4,14 @@ require "palindrome_ext/string"
 
 # Overrides Default Array Class by adding {palindrome?} method.
 class Array
+  # @param strict whether or not to ignore whitespace and non-alphanumeric characters.
   # @return [Boolean] Whether or not the array is a palindrome.
-  def palindrome?
-    self == reverse
+  def palindrome?(strict = true)
+    return self == reverse if strict
+
+    arr = map { |x| x.to_s.downcase.tr("^a-z0-9", "") }
+    arr.delete_if(&:empty?)
+    puts arr
+    arr == arr.reverse
   end
 end
